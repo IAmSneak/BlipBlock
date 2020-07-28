855ac015eaa8f1b62ba80429ba368a5d17e4dd90
IAmSneak <66091019+IAmSneak@users.noreply.github.com>
2020-07-27 22:10:45 -0400

    Update BlockBase.java

--- src/main/java/com/gmail/sneakdevs/content/types/BlockBase.java
+++ src/main/java/com/gmail/sneakdevs/content/types/BlockBase.java
@@ -3,7 +3,6 @@ package com.gmail.sneakdevs.content.types;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
-import net.minecraft.block.RedstoneLampBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
