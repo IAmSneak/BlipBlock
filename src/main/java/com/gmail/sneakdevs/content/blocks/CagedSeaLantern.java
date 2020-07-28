527b0601d04155c760b70f092f9c45f92ed2a983
IAmSneak <66091019+IAmSneak@users.noreply.github.com>
2020-07-28 16:37:25 -0400

    Added Stained Caged Sea Lanterns

--- src/main/java/com/gmail/sneakdevs/content/blocks/CagedSeaLantern.java
+++ src/main/java/com/gmail/sneakdevs/content/blocks/CagedSeaLantern.java
@@ -8,11 +8,11 @@ import net.minecraft.sound.BlockSoundGroup;
 
 public class CagedSeaLantern {
 
-    public static final LightBlockBase CAGED_PRISMARINE_LANTERN = new LightBlockBase("caged_prismarine_lantern", 1.2f, 0.6f, Material.STONE, BlockSoundGroup.GLASS, ItemGroup.BUILDING_BLOCKS, FabricToolTags.PICKAXES, 0, 14);
-    public static final LightBlockBase RED_CAGED_PRISMARINE_LANTERN = new LightBlockBase("red_caged_prismarine_lantern", 1.2f, 0.6f, Material.STONE, BlockSoundGroup.GLASS, ItemGroup.BUILDING_BLOCKS, FabricToolTags.PICKAXES, 0, 14);
-    public static final LightBlockBase LIME_PRISMARINE_LANTERN = new LightBlockBase("lime_caged_prismarine_lantern", 1.2f, 0.6f, Material.STONE, BlockSoundGroup.GLASS, ItemGroup.BUILDING_BLOCKS, FabricToolTags.PICKAXES, 0, 14);
-    public static final LightBlockBase YELLOW_CAGED_PRISMARINE_LANTERN = new LightBlockBase("yellow_caged_prismarine_lantern", 1.2f, 0.6f, Material.STONE, BlockSoundGroup.GLASS, ItemGroup.BUILDING_BLOCKS, FabricToolTags.PICKAXES, 0, 14);
-    public static final LightBlockBase ORANGE_CAGED_PRISMARINE_LANTERN = new LightBlockBase("orange_caged_prismarine_lantern", 1.2f, 0.6f, Material.STONE, BlockSoundGroup.GLASS, ItemGroup.BUILDING_BLOCKS, FabricToolTags.PICKAXES, 0, 14);
-    public static final LightBlockBase PURPLE_CAGED_PRISMARINE_LANTERN = new LightBlockBase("purple_caged_prismarine_lantern", 1.2f, 0.6f, Material.STONE, BlockSoundGroup.GLASS, ItemGroup.BUILDING_BLOCKS, FabricToolTags.PICKAXES, 0, 14);
+    public static final LightBlockBase CAGED_SEA_LANTERN = new LightBlockBase("caged_sea_lantern", 1.2f, 0.6f, Material.STONE, BlockSoundGroup.GLASS, ItemGroup.BUILDING_BLOCKS, FabricToolTags.PICKAXES, 0, 14);
+    public static final LightBlockBase RED_CAGED_SEA_LANTERN = new LightBlockBase("red_caged_sea_lantern", 1.2f, 0.6f, Material.STONE, BlockSoundGroup.GLASS, ItemGroup.BUILDING_BLOCKS, FabricToolTags.PICKAXES, 0, 14);
+    public static final LightBlockBase LIME_CAGED_SEA_LANTERN = new LightBlockBase("lime_caged_sea_lantern", 1.2f, 0.6f, Material.STONE, BlockSoundGroup.GLASS, ItemGroup.BUILDING_BLOCKS, FabricToolTags.PICKAXES, 0, 14);
+    public static final LightBlockBase YELLOW_CAGED_SEA_LANTERN = new LightBlockBase("yellow_caged_sea_lantern", 1.2f, 0.6f, Material.STONE, BlockSoundGroup.GLASS, ItemGroup.BUILDING_BLOCKS, FabricToolTags.PICKAXES, 0, 14);
+    public static final LightBlockBase ORANGE_CAGED_SEA_LANTERN = new LightBlockBase("orange_caged_sea_lantern", 1.2f, 0.6f, Material.STONE, BlockSoundGroup.GLASS, ItemGroup.BUILDING_BLOCKS, FabricToolTags.PICKAXES, 0, 14);
+    public static final LightBlockBase PURPLE_CAGED_SEA_LANTERN = new LightBlockBase("purple_caged_sea_lantern", 1.2f, 0.6f, Material.STONE, BlockSoundGroup.GLASS, ItemGroup.BUILDING_BLOCKS, FabricToolTags.PICKAXES, 0, 14);
 
 }
