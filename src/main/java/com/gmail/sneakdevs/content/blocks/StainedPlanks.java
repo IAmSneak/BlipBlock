818373544b479ded9536a3765aa6f6aadcd2af26
IAmSneak <66091019+IAmSneak@users.noreply.github.com>
2020-07-31 19:23:34 -0400

    Added Stained Slabs and Fences

--- src/main/java/com/gmail/sneakdevs/content/blocks/StainedPlanks.java
+++ src/main/java/com/gmail/sneakdevs/content/blocks/StainedPlanks.java
@@ -1,6 +1,8 @@
 package com.gmail.sneakdevs.content.blocks;
 
+import com.gmail.sneakdevs.content.types.FenceBase;
 import com.gmail.sneakdevs.content.types.HandBlockBase;
+import com.gmail.sneakdevs.content.types.SlabBase;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Material;
 import net.minecraft.item.ItemGroup;
@@ -9,20 +11,54 @@ import net.minecraft.sound.BlockSoundGroup;
 public class StainedPlanks {
 
     public static final HandBlockBase BLACK_STAINED_PLANKS = new HandBlockBase("black_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
-    public static final HandBlockBase RED_STAINED_PLANKS = new HandBlockBase("red_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES, 0);
-    public static final HandBlockBase GREEN_STAINED_PLANKS = new HandBlockBase("green_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
-    public static final HandBlockBase BROWN_STAINED_PLANKS = new HandBlockBase("brown_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
-    public static final HandBlockBase BLUE_STAINED_PLANKS = new HandBlockBase("blue_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
-    public static final HandBlockBase PURPLE_STAINED_PLANKS = new HandBlockBase("purple_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
-    public static final HandBlockBase CYAN_STAINED_PLANKS = new HandBlockBase("cyan_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
     public static final HandBlockBase GRAY_STAINED_PLANKS = new HandBlockBase("gray_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
+    public static final HandBlockBase LIGHT_GRAY_STAINED_PLANKS = new HandBlockBase("light_gray_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
+    public static final HandBlockBase WHITE_STAINED_PLANKS = new HandBlockBase("white_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
+    public static final HandBlockBase RED_STAINED_PLANKS = new HandBlockBase("red_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES, 0);
+    public static final HandBlockBase ORANGE_STAINED_PLANKS = new HandBlockBase("orange_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
+    public static final HandBlockBase YELLOW_STAINED_PLANKS = new HandBlockBase("yellow_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
+    public static final HandBlockBase GREEN_STAINED_PLANKS = new HandBlockBase("green_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
+    public static final HandBlockBase BLUE_STAINED_PLANKS = new HandBlockBase("blue_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
+    public static final HandBlockBase CYAN_STAINED_PLANKS = new HandBlockBase("cyan_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
+    public static final HandBlockBase PURPLE_STAINED_PLANKS = new HandBlockBase("purple_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
+    public static final HandBlockBase BROWN_STAINED_PLANKS = new HandBlockBase("brown_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
     public static final HandBlockBase PINK_STAINED_PLANKS = new HandBlockBase("pink_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES, 0);
     public static final HandBlockBase LIME_STAINED_PLANKS = new HandBlockBase("lime_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
-    public static final HandBlockBase YELLOW_STAINED_PLANKS = new HandBlockBase("yellow_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
     public static final HandBlockBase MAGENTA_STAINED_PLANKS = new HandBlockBase("magenta_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
-    public static final HandBlockBase ORANGE_STAINED_PLANKS = new HandBlockBase("orange_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
-    public static final HandBlockBase WHITE_STAINED_PLANKS = new HandBlockBase("white_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
     public static final HandBlockBase LIGHT_BLUE_STAINED_PLANKS = new HandBlockBase("light_blue_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
-    public static final HandBlockBase LIGHT_GRAY_STAINED_PLANKS = new HandBlockBase("light_gray_stained_planks", 2.0f, 3.0f, Material.WOOD, BlockSoundGroup.WOOD, ItemGroup.BUILDING_BLOCKS, FabricToolTags.AXES,  0);
+
+    public static final SlabBase BLACK_STAINED_PLANK_SLAB = new SlabBase("black_stained_plank_slab", BLACK_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase GRAY_STAINED_PLANK_SLAB = new SlabBase("gray_stained_plank_slab", GRAY_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase LIGHT_GRAY_STAINED_PLANK_SLAB = new SlabBase("light_gray_stained_plank_slab", LIGHT_GRAY_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase WHITE_STAINED_PLANK_SLAB = new SlabBase("white_stained_plank_slab", WHITE_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase RED_STAINED_PLANK_SLAB = new SlabBase("red_stained_plank_slab", RED_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase ORANGE_STAINED_PLANK_SLAB = new SlabBase("orange_stained_plank_slab", ORANGE_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase YELLOW_STAINED_PLANK_SLAB = new SlabBase("yellow_stained_plank_slab", YELLOW_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase GREEN_STAINED_PLANK_SLAB = new SlabBase("green_stained_plank_slab", GREEN_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase BLUE_STAINED_PLANK_SLAB = new SlabBase("blue_stained_plank_slab", BLUE_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase CYAN_STAINED_PLANK_SLAB = new SlabBase("cyan_stained_plank_slab", CYAN_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase PURPLE_STAINED_PLANK_SLAB = new SlabBase("purple_stained_plank_slab", PURPLE_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase BROWN_STAINED_PLANK_SLAB = new SlabBase("brown_stained_plank_slab", BROWN_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase PINK_STAINED_PLANK_SLAB = new SlabBase("pink_stained_plank_slab", PINK_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase LIME_STAINED_PLANK_SLAB = new SlabBase("lime_stained_plank_slab", LIME_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase MAGENTA_STAINED_PLANK_SLAB = new SlabBase("magenta_stained_plank_slab", MAGENTA_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+    public static final SlabBase LIGHT_BLUE_STAINED_PLANK_SLAB = new SlabBase("light_blue_stained_plank_slab", LIGHT_BLUE_STAINED_PLANKS, ItemGroup.BUILDING_BLOCKS);
+
+    public static final FenceBase BLACK_STAINED_PLANK_FENCE = new FenceBase("black_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase GRAY_STAINED_PLANK_FENCE = new FenceBase("gray_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase LIGHT_GRAY_STAINED_PLANK_FENCE = new FenceBase("light_gray_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase WHITE_STAINED_PLANK_FENCE = new FenceBase("white_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase RED_STAINED_PLANK_FENCE = new FenceBase("red_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase ORANGE_STAINED_PLANK_FENCE = new FenceBase("orange_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase YELLOW_STAINED_PLANK_FENCE = new FenceBase("yellow_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase GREEN_STAINED_PLANK_FENCE = new FenceBase("green_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase BLUE_STAINED_PLANK_FENCE = new FenceBase("blue_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase CYAN_STAINED_PLANK_FENCE = new FenceBase("cyan_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase PURPLE_STAINED_PLANK_FENCE = new FenceBase("purple_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase BROWN_STAINED_PLANK_FENCE = new FenceBase("brown_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase PINK_STAINED_PLANK_FENCE = new FenceBase("pink_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase LIME_STAINED_PLANK_FENCE = new FenceBase("lime_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase MAGENTA_STAINED_PLANK_FENCE = new FenceBase("magenta_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
+    public static final FenceBase LIGHT_BLUE_STAINED_PLANK_FENCE = new FenceBase("light_blue_stained_plank_fence", ItemGroup.BUILDING_BLOCKS);
 
 }
